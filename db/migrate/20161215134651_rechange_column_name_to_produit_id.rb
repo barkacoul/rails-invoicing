c5020c483dd3d380860a9d71bd31e44fafb41a60
Pape Barka Seydou Coulibaly <papebarka@gmail.com>
2017-02-25 23:54:08 +0000

    r_version

--- db/migrate/20161215134651_rechange_column_name_to_produit_id.rb
+++ db/migrate/20161215134651_rechange_column_name_to_produit_id.rb
@@ -1,5 +1,5 @@
 class RechangeColumnNameToProduitId < ActiveRecord::Migration
   def change
-      rename_column :facture_produits, :produdit_id, :produit_id
+      #rename_column :facture_produits, :produdit_id, :produit_id
   end
 end
