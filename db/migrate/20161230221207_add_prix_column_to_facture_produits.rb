f78c8d01b1add3184d25a97576f9537a9237587a
Pape Barka Seydou Coulibaly <papebarka@gmail.com>
2017-02-26 09:23:31 +0000

    modified

--- db/migrate/20161230221207_add_prix_column_to_facture_produits.rb
+++
@@ -1,6 +0,0 @@
-class AddPrixColumnToFactureProduits < ActiveRecord::Migration
-  def change
-      add_column :facture_produits, :sous_total, :float
-       add_index   :entreprises, :utilisateur_id
-  end
-end
