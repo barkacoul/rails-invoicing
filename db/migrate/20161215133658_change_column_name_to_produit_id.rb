19b91f40d3eaedbd12ab0f1b5873b9f9a2ea49e5
Pape Barka Seydou Coulibaly <papebarka@gmail.com>
2017-02-26 09:13:06 +0000

    removed change produit_id name

--- db/migrate/20161215133658_change_column_name_to_produit_id.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnNameToProduitId < ActiveRecord::Migration
-  def change
-      rename_column :facture_produits, :produit_id, :produdit_id
-      #add_index :facture_produits, :produit_id
-  end
-end
